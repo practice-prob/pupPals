ac19468865c5008efef8fa27730d2c3ca1b57965
winniemei <wmei91@gmail.com>
2023-08-03 15:54:20 -0400

    render pup data

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+
+  }
+  
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div className="App">
+        {featPupId && 
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>}
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
         })}
       </div>
     </>
